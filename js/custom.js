e06257813fe2dc34bb38e70a6720c1c8d28b2029
salkarprashant <prashant.milestone@gmail.com>
2022-06-28 23:47:10 +0530

    anahit_html

---
+++ js/custom.js
@@ -0,0 +1,42 @@
+//Animation JS
+AOS.init(); 
+
+
+//Sidebar Menu JS
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e) => {
+    let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
+    arrowParent.classList.toggle("showMenu");
+  });
+}
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("side-close");
+});
+
+// Grid and List JS
+function showList(e) {
+  var $gridCont = $('.grid-container');
+  e.preventDefault();
+  $gridCont.hasClass('list-view') ? $gridCont.removeClass('list-view') : $gridCont.addClass('list-view');
+}
+function gridList(e) {
+  var $gridCont = $('.grid-container')
+  e.preventDefault();
+  $gridCont.removeClass('list-view');
+}
+
+$(document).on('click', '.btn-grid', gridList);
+$(document).on('click', '.btn-list', showList);
+
+ $(".btn-list").click(function(){
+       $(".btn-list").addClass("active");
+       $(".btn-grid").removeClass("active");
+    });
+ $(".btn-grid").click(function(){
+       $(".btn-grid").addClass("active");
+       $(".btn-list").removeClass("active");
+    });
